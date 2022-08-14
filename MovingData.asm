7b0ab2c7ae30187e5b4e8acf524b8c39ec329c21
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2022-08-14 16:02:32 +0100

    Create MovingData.asm

---
+++ MovingData.asm
@@ -0,0 +1,15 @@
+; MovingData.asm
+; Alexander Walford 2022
+
+section .text
+    global _start ; for gcc
+
+_start:
+    ; Task 1: Define 2 values, then sum them together into a single register
+    set    %rax, 2
+    set    %rcx, 8
+    add    %rax    %rcx
+
+    ; Task 2: Move the value of the sum into a new register
+    mov    %rcx    %rdx
+
